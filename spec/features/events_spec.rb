dfdabef9469c00e69dc37bea5a4f3ed733146288
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-19 13:38:34 -0700

    all specs green

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 feature 'Events' do
 
+  scenario 'User sees headline' do
+
+    visit root_path
+    expect(page).to have_content('Events')
+  end
+
+  scenario 'User can see a list of events' do
+    Event.create!(description: 'My awesome event')
+
+    visit root_path
+    expect(page).to have_content('My awesome event')
+  end
+
+
   scenario 'User edits an event' do
     Event.create!(
       description: 'My aweeesome event'
@@ -16,6 +30,16 @@ feature 'Events' do
     expect(page).to have_content('My awesome event')
     expect(page).to have_no_content('My aweeesome event')
   end
+
+  scenario 'User can delete an event' do
+    Event.create!(description: 'My aweeesome event')
+
+    visit root_path
+    expect(page).to have_content('My aweeesome event')
+    click_on 'delete'
+    expect(page).to have_no_content('My aweeesome event')
+  end
+
 
 
 
